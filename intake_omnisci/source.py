84a23f38e78ba7d9f739c940eafd75b7044e5c4d
Ian Rose <ian.r.rose@gmail.com>
2019-07-25 15:57:03 -0700

    Rationalize connection parameters with those used in pymapd and ibis.

--- intake_omnisci/source.py
+++ intake_omnisci/source.py
@@ -17,44 +17,80 @@ class OmniSciSource(DataSource):
     container = "dataframe"
     partition_access = False
 
-    def __init__(self, uri, collection, projection=None, metadata=None):
-        """Load data from OmniSci
+    def __init__(
+        self,
+        sql_expr,
+        uri=None,
+        user=None,
+        password=None,
+        host=None,
+        port=6274,
+        dbname=None,
+        protocol="binary",
+        metadata=None,
+    ):
+        """
+        Load data from an OmniSci database.
+        Can take *either* a SQLAlchemy-compliant connection string,
+        or a set uf user, password, host, port, dbname.
 
         Parameters:
+            sql_expr: str
+                The data to query from the database. Can either be a table name
+                or a SQL query.
             uri: str
-                a valid OmniSci uri in the form 'protocol://host:port/database'.
-            collection: str
-                The collection in the database that will act as a source.
-                Can either be a table name or a SQL query.
-            projection: tuple/list
-                The fields to query.
+                a valid OmniSci uri in the form 'mapd://user:password@host:port/database'.
+            user: str
+                The user name.
+            password: str
+                The user password.
+            host: str
+                The hostname for the database server.
+            port: int
+                The port for the database server.
+            dbname: str
+                The name of the database to connect to.
+            protocol: str
+                The protocol for the database server.
             metadata: dict
-                The metadata to keep
+                The metadata for the source.
         """
 
         self._uri = uri
-        self._collection = collection
-        self._projection = projection
+        self._user = user
+        self._password = password
+        self._host = host
+        self._port = port
+        self._dbname = dbname
+        self._protocol = protocol
+        self._sql_expr = sql_expr
         self._dtypes = None
         self._connection = None
 
         super().__init__(metadata=metadata)
 
-    def _make_connection(self):
-        select_cmd = "SELECT {} FROM {} LIMIT 10".format(
-            ", ".join(self._projection or ["*"]), self._collection
-        )
-        self._connection = pymapd.connect(self._uri).execute(select_cmd)  # Cursor
+    def _make_cursor(self):
+        if not self._connection:
+            self._connection = pymapd.connect(
+                uri=self._uri,
+                user=self._user,
+                password=self._password,
+                host=self._host,
+                port=self._port,
+                protocol=self._protocol,
+                dbname=self._dbname,
+            )
+        return self._connection.execute(self._sql_expr)
 
     def _get_schema(self):
-        if self._connection is None:
-            self._make_connection()
-
         if self._dtypes is None:
-            # TODO: use pymapd tools to fill dtypes
-            self._dtypes = pandas.DataFrame.from_records(
-                self._connection.fetchmany(1)
-            ).dtypes
+            cursor = self._make_cursor()
+
+            # TODO: pymapd provides some data for querying column types.
+            # Can we use that here? This requires an extra fetch, but
+            # should work for more complex queries.
+            self._dtypes = pandas.DataFrame.from_records(cursor.fetchmany(1)).dtypes
+            cursor.close()
 
         return Schema(
             datashape="datashape",
@@ -66,9 +102,11 @@ class OmniSciSource(DataSource):
 
     def _get_partition(self, _):
         self._get_schema()
-        return pandas.DataFrame.from_records(
-            self._connection.fetchall(), columns=self._projection
-        )
+        cursor = self._make_cursor()
+        columns = [d.name for d in cursor.description]
+        records = cursor.fetchall()
+        cursor.close()
+        return pandas.DataFrame.from_records(records, columns=columns)
 
     def read(self):
         self._dataframe = self._get_partition(0)
@@ -76,7 +114,7 @@ class OmniSciSource(DataSource):
 
     def _close(self):
         # close any files, sockets, etc
-        if self._connection is not None:
+        if self._connection:
             self._connection.close()
             self._connection = None
         self._dataframe = None
