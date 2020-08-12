e8ce193629cf8ee075d0d9d896f4412331f5d2a9
Kim Pevey <kcpevey@gmail.com>
2020-08-12 07:53:52 -0500

    bumped version to 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="intake-omnisci",
-    version='0.0.1',
+    version='0.0.2',
     description="OmniSci plugin for Intake",
     url="https://github.com/Quansight/intake-omnisci",
     maintainer="Pearu Peterson and Ian Rose",
